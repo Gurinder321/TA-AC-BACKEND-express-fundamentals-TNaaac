3b4f1c5dbab50028152098d6a28cb9c48591dc75
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-04-14 10:11:27 -0700

    done

--- block-BNaabv/assignment1/server.js
+++ block-BNaabv/assignment1/server.js
@@ -1,37 +1,45 @@
 const express = require('express');
 const PORT = 3000;
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/public'));
+
+app.use(cookieParser());
 app.use(logger('dev'));
+
+app.use((req, res, next) => {
+  res.cookie('username', 'xyz');
+  next();
+});
 
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.send('<h2>Welcome to express</h2>');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/form', (req, res) => {
-  res.sendFile(__dirname + '/form.html');
+  res.send('My name is qwerty');
 });
 
 app.post('/form', (req, res) => {
-  console.log(req.body);
+  res.json(req.body);
 });
 
 app.post('/json', (req, res) => {
   console.log(req.body);
 });
 
+app.get('/users/:username', (req, res) => {
+  const username = req.params.username;
+  res.send(`<h2>${username}</h2>`);
+});
+
+// 404 middleware
 app.use((req, res, next) => {
-  console.log(err);
   res.send('Page not found');
 });
 
